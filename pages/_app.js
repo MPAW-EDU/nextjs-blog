3c50a874768a91bb6a1aad62f64374cbb79f0ad5
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-20 15:31:00 -0700

    Scripted the overall layout using Tailwind, continuing to style.

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,17 @@ import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-9/12 my-8">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Blog</h1>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li className=""><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   )
