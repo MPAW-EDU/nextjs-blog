caad6a0854e1db50a09b56848d46c77bc9a74289
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-20 14:04:11 -0700

    Stringified date in the data object. Mapped out posts in the indexand made a dynamic component that can render through the router based on params. Squashed all of my bugs and all is working.

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
 export const blogPosts = [
     {
         title: 'First Post',
-        date: new Date(),
+        slug: 'first',
+        date: new Date().toString(),
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         title: 'Second Post',
-        date: new Date(),
+        slug: 'second',
+        date: new Date().toString(),
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         title: 'Third Post',
-        date: new Date(),
+        slug: 'third',
+        date: new Date().toString(),
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
 ]
