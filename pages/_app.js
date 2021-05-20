f725cd5843e82a3d594827fc3ddb950bd1533ae3
Michael Walter <michael_patrick.walter@outlook.com>
2021-05-19 22:36:53 -0700

    Added nav, about page, and testing data object.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
